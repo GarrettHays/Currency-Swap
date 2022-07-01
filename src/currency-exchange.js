61eaad4c57f254789e9ca1a94090664055642d04
Garrett Hays <GarrettlHays@gmail.com>
2022-07-01 10:12:10 -0700

    adds biz logic js file for API call

---
+++ src/currency-exchange.js
@@ -0,0 +1,17 @@
+export default class CurrencyExchange {
+  static getExchange(currencyAmt, currencyFrom, currencyTo) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${currencyAmt}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
